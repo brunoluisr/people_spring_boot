73b6d21d2b2a8b547ef42805b9b3f08f61a5ccbe
Bruno Ribeiro <blbribeirodb@gmail.com>
2022-10-01 08:39:15 +0100

    SpringBoot

---
+++ People-MS/src/main/java/BLBR/REPRO/COM/PeopleMS/PeopleMsApplication.java
@@ -0,0 +1,13 @@
+package BLBR.REPRO.COM.PeopleMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleMsApplication.class, args);
+	}
+
+}
